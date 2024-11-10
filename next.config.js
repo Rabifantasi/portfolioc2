38452b2ff9318c823149c46a6c8e5bc42b8536d6
Rabia Farooq Shaikh <159634085+Rabifantasi@users.noreply.github.com>
2024-11-10 22:41:02 +0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,10 @@
+<<<<<<< HEAD
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var nextConfig = {
+/* config options here */
+};
+exports.default = nextConfig;
+=======
+export const reactStrictMode = true;
+>>>>>>> b9850d41bb389573af4af001f6da6d4ce9406bcf
